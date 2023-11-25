5dff7a621bb1ad78b150546de26ff118dd052e2b
monab3 <you@example.com>
2023-11-25 19:19:19 +0100

    25112023 slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { rebsortenService } from '../shared/services/rebsorten.service';
 import { base64ToImageConverter } from '../shared/services/base64ToImageConverter.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -27,29 +28,35 @@ blurDivs.forEach(div =>{
 
 export class AppComponent implements OnInit {
   title = 'store';
-  base64String: string |undefined ;
-
+  base64String: string | undefined;
 
   constructor(private rebsortenService: rebsortenService, private imageTransformSerivce: base64ToImageConverter) {
-   } 
-
-  ngOnInit(): void {
-    console.log("test");
   }
+  formGeschmack = new FormGroup({
+    geschmack: new FormControl(''),
+  });
 
-  public fetchDatafromBackend(): void{
-      this.rebsortenService.fetchRebsorten().subscribe((data)=>{
-        if(data){
+  formRebsorte = new FormGroup({
+    rebsorte: new FormControl(''),
+  });
+  ngOnInit(): void {
+    this.formGeschmack.get('geschmack')?.valueChanges.subscribe((value) => {
+      console.log('Selected Geschmack:', value);
+    });
+  }
+  public fetchDatafromBackend(): void {
+    this.rebsortenService.fetchRebsorten().subscribe((data) => {
+      if (data) {
 
-          for (let item of data) {
-           if(item.rebsorteImage){
+        for (let item of data) {
+          if (item.rebsorteImage) {
             this.base64String = this.imageTransformSerivce.transform(item.rebsorteImage, 'image/png');
             console.log("test");
-           }
           }
-
         }
-      })
+
+      }
+    })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/*Angular Components */
+import { AccordionModule } from 'primeng/accordion';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +19,10 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
