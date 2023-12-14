a274f4690296b8c7ee17f9a46bcd1094dbcc526a
monab3 <you@example.com>
2023-12-14 20:24:04 +0100

    grid inklusion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductViewComponent } from './product-view/product-view.component';
 import { WeinDetailseiteComponent } from './wein-detailseite/wein-detailseite.component';
+import { TestofresponsiveComponent } from './testofresponsive/testofresponsive.component';
 
 const routes: Routes = [
   { path: '', component: ProductViewComponent },
